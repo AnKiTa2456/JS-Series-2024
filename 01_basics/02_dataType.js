4324e481ee2eb16977b415b6ea7e3bcf42ff97eb
Ankita Patel <114217727+AnKiTa2456@users.noreply.github.com>
2024-05-30 19:33:58 +0000

    data type

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,26 @@
+"use strict" // treat al JS code as newer version
+
+//alert(3+2)    // we are using nidejs, not browser
+
+let age =20
+let name = "Ankita"
+let logedIn =false
+
+console.log(3+3) // code readbility should be high
+
+
+
+// number = 3
+// bigint
+//string => " "
+//null=> standalone value // it is representation of empty value // it is specal type
+//undefind => not dassign the value
+//boolean => true/ false
+//symbol => uniqe
+
+//object
+console.log(typeof "Ankita") //string
+console.log(typeof age)   // NUMBER
+console.log(typeof null)  //OBJECT
+console.log(typeof undefined)  //UNDEFIND
+console.log(typeof Symbol) // FUNCTION
