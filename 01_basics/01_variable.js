c042e493014d0e51f04453b0febde5adba3c70af
Ankita Patel <114217727+AnKiTa2456@users.noreply.github.com>
2024-05-30 18:51:40 +0000

    knowledge of variable

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,1 +1,18 @@
 console.log("ankita")
+const accountId = 8345984
+let accountEmail = "ankita@gmail.com"
+var accountPassword = "798uh"
+accountCity = "Lucknow"
+
+//accountId = 557 // not allowed
+console.log(accountId)
+/*
+ Prefer not to use var
+ because of issue in block scope and functional scope
+*/
+
+accountEmail = "patel@gmail.com"
+accountPassword = "hdfjky"
+accountCity = "Mirzapur"
+
+console.table([accountEmail, accountPassword, accountId, accountPassword])
